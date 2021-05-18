d1e7c8bd9ffebbb8c20ec39fe31c66537091a3a7
etnflash <84004528+etnflash@users.noreply.github.com>
2021-05-18 18:14:21 +0900

    Add files via upload

---
+++ exam1.java
@@ -0,0 +1,20 @@
+package assignment0518;
+
+public class exam1 {
+	public static void main(String args[]) {
+		int kor, mat, eng, sum, avg;
+		
+		kor = 90;
+		mat = 80;
+		eng = 100;
+		
+		sum = kor + mat + eng;
+		avg = sum / 3;
+		
+		System.out.println("kor "+kor);
+		System.out.println("mat "+mat);
+		System.out.println("eng "+eng);
+		System.out.println("sum "+sum);
+		System.out.println("avg "+avg);
+	}
+}
---
+++ exam2.java
@@ -0,0 +1,7 @@
+package assignment0518;
+
+public class exam2 {
+	public static void main(String args[]) {
+		System.out.println("TTTTTTTTTT\nTTTTTTTTTT\n    TT    \n    TT    \n    TT    ");
+	}
+}
