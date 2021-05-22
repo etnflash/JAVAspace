cc9dee5da9a8adfdc98fe98737682d7426b6dfa2
etnflash <84004528+etnflash@users.noreply.github.com>
2021-05-22 19:57:47 +0900

    Update and rename exam2.java to question2.java

--- question2.java
+++ question2.java
@@ -1,6 +1,6 @@
 package assignment0518;
 
-public class exam2 {
+public class question2 {
 	public static void main(String args[]) {
 		System.out.println("TTTTTTTTTT\nTTTTTTTTTT\n    TT    \n    TT    \n    TT    ");
 	}
