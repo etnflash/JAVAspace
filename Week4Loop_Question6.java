fd5859cb4f779780040eadafb976f12ef24cbf46
etnflash <84004528+etnflash@users.noreply.github.com>
2021-06-08 06:04:21 +0900

    Add files via upload
    Week4Loop_Assignment

---
+++ Week4Loop_Question6.java
@@ -0,0 +1,12 @@
+package assignment0601;
+import java.util.Scanner;
+public class Week4Loop_Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		
+	}
+
+}
