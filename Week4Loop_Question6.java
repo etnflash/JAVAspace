2afce31b0330c5e1438dd3d0217ff29b13fad69e
etnflash <84004528+etnflash@users.noreply.github.com>
2021-06-08 14:42:32 +0900

    Add files via upload

--- Week4Loop_Question6.java
+++ Week4Loop_Question6.java
@@ -6,7 +6,21 @@ public class Week4Loop_Question6 {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		
-		
-	}
+		String c;
+		do
+		{	
+			System.out.println("Triangle Width? ");
+			System.out.print("base? ");
+			double base = scan.nextDouble();
+			System.out.print("height? ");
+			double height = scan.nextDouble();
 
+			System.out.printf("%.1f\n", base*height/2);
+
+			System.out.print("continue? ");
+			c = scan.nextLine();
+			c = scan.nextLine();
+	} while ( c.equals("y") || c.equals("Y"));
+
+}
 }
